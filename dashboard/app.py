3cd86db9db9a4010bb5adeb79896535998c52498
Luc Setzer <lucsetzer@gmail.com>
2026-01-27 18:09:12 -0500

    Add Response import to app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form, Cookie
+from fastapi import FastAPI, Request, Form, Cookie, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import sqlite3
